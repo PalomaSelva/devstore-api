3d3946b61fb14d5f7b752d202cdb60dba2bb4321
PalomaSelva <palomaselva1@gmail.com>
2024-07-24 13:35:29 -0300

    ajustando prfixo de rota

--- src/app/data/api.ts
+++ src/app/data/api.ts
@@ -4,6 +4,6 @@ export async function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
   const apiPrefix = '/data/api'
   const url = new URL(apiPrefix.concat(path), baseUrl)
-
+  console.log(url)
   return fetch(url, init)
 }
